ac20a3e05516f2bbaa6ee42c81032e15e39004d0
Micherre Fox <micherrefox@gmail.com>
2020-07-21 15:14:49 -0700

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
 import SavedList from './Movies/SavedList';
+import { Route, Link, Switch } from 'react-router-dom'
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
+
 
 const App = () => {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -27,10 +30,26 @@ const App = () => {
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
-      <div>Replace this Div with your Routes</div>
+        <SavedList list={[ /* This is stretch */]} />
+        <div>
+          <Link to='/'>Movie List</Link>
+        </div>
+      <Switch>
+        
+        <Route path='/movies/:id'>
+          <Movie />
+        </Route>
+
+        <Route path='/'>
+          <MovieList movies={movieList}/>
+        </Route>
+      </Switch>
     </div>
   );
 };
 
 export default App;
+
+
+//[ ] one route for `/` that loads the `MovieList` component. **This component will need the movies injected into it via props**.
+//* [ ] one route that will take an `id` parameter after`/movies/` (ex: `/movies/2`, `/movies/3` where the id is dynamic). This route should load the `Movie` component.
